61186667bb7b27ca7c62d96cd9f1b638fbbf9d7a
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-09-05 11:18:19 -0300

    ap08(blocos-anonimos-exercicio): exercicio 1.3

--- script_exercicio.sql
+++ script_exercicio.sql
@@ -1,16 +1,32 @@
--- Exercicio 1
 DO 
 $$
-DECLARE 
-	n_inteiros INTEGER;
-	n_reais NUMERIC (5, 2);
-BEGIN 
-	--1.1 Faça um programa que gere um valor inteiro e o exiba.
-	n_inteiros := 1 + random() * 100::INT;
-	RAISE NOTICE 'Números Inteiros: %', n_inteiros;
-	
-	-- 1.2. Faça um programa que gere um valor real e o exiba.
-	n_reais := 1 + random() * 10;
-	RAISE NOTICE 'Números Reais: %', n_reais;
+DECLARE
+	celsius NUMERIC (5, 2);
+	fahrenheit NUMERIC (5, 2);
+	limite_inferior NUMERIC (5, 2) := 20;
+	limite_superior NUMERIC (5, 2) := 30;
+BEGIN
+	celsius := limite_inferior + random() * (limite_superior - limite_inferior + 1);
+	fahrenheit := (celsius * 9/5) + 32;
+	RAISE NOTICE 'Temperatura em celsuis: %. Temperatura em fahrenheit: %', celsius, fahrenheit;
 END;
 $$
+
+
+-- -- Exercicio 1
+-- DO 
+-- $$
+-- DECLARE 
+-- 	n_inteiros INTEGER;
+-- 	n_reais NUMERIC (5, 2);
+-- BEGIN 
+-- 	--1.1 Faça um programa que gere um valor inteiro e o exiba.
+-- 	n_inteiros := 1 + random() * 100::INT;
+-- 	RAISE NOTICE 'Números Inteiros: %', n_inteiros;
+	
+-- 	-- 1.2. Faça um programa que gere um valor real e o exiba.
+-- 	n_reais := 1 + random() * 10;
+-- 	RAISE NOTICE 'Números Reais: %', n_reais;
+	
+-- END;
+-- $$
