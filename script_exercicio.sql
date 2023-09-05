3f09306b0d15f01cfaa7ada14287a6e7a6ccec6a
Maria Eduarda Ribeiro <maria.ribeiro33@fatec.sp.gov.br>
2023-09-05 11:24:42 -0300

    ap08(blocos-anonimos-exercicio): exercicio 1.4

--- script_exercicio.sql
+++ script_exercicio.sql
@@ -1,16 +1,32 @@
 DO 
 $$
 DECLARE
-	celsius NUMERIC (5, 2);
-	fahrenheit NUMERIC (5, 2);
-	limite_inferior NUMERIC (5, 2) := 20;
-	limite_superior NUMERIC (5, 2) := 30;
+	a NUMERIC (5, 2);
+	b NUMERIC (5, 2);
+	c NUMERIC (5, 2);
+	delta NUMERIC (5, 2);
 BEGIN
-	celsius := limite_inferior + random() * (limite_superior - limite_inferior + 1);
-	fahrenheit := (celsius * 9/5) + 32;
-	RAISE NOTICE 'Temperatura em celsuis: %. Temperatura em fahrenheit: %', celsius, fahrenheit;
+	a := random() * 10;
+	b := random() * 10;
+	c := random() * 10;
+	delta := b^2 - 4 * a * c;
+	RAISE NOTICE 'A: %. B: %. C: %. Delta é: %', a, b, c, delta;
 END;
 $$
+
+-- DO 
+-- $$
+-- DECLARE
+-- 	celsius NUMERIC (5, 2);
+-- 	fahrenheit NUMERIC (5, 2);
+-- 	limite_inferior NUMERIC (5, 2) := 20;
+-- 	limite_superior NUMERIC (5, 2) := 30;
+-- BEGIN
+-- 	celsius := limite_inferior + random() * (limite_superior - limite_inferior + 1);
+-- 	fahrenheit := (celsius * 9/5) + 32;
+-- 	RAISE NOTICE 'Temperatura em celsuis: %. Temperatura em fahrenheit: %', celsius, fahrenheit;
+-- END;
+-- $$
 
 
 -- -- Exercicio 1
